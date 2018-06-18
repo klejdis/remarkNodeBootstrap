b81bb960f957eeee876652aaa0714021552111fb
Klejdis Jorgji <klejdis.jorgji@atis.al>
2018-06-18 15:58:20 +0200

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+// @see: https://gist.github.com/branneman/8048520
+require('app-module-path').addPath(__dirname + '/lib');
+
+var server = require('nodebootstrap-server')
+  , appConfig = require('./appConfig')
+  , app    = require('express')();
+
+
+//CONFIGURE HTML APP GENERAL SETTINGS
+app = require('nodebootstrap-htmlapp').setup(app);
+
+
+server.setup(app, appConfig.setup);
